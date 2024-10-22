629333d119129187afe8b930f3a61cf7f25fc41a
Jormart <jmartineztortola@gmail.com>
2024-10-23 00:46:02 +0200

    first commit

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,44 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.domain.price.Price;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PricingServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void getPrice() {
+		long vehicleId = ThreadLocalRandom.current().nextLong(1, 20);
+		ResponseEntity<Price> response =
+				this.testRestTemplate.getForEntity("http://localhost:" + port + "services/price/" + vehicleId, Price.class);
+
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+	}
+
+}
